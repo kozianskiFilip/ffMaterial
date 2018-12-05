fc8fc147bdf4ea6ee5197c57cff5280e327efe3c
Filip Koziański <filip.kozianski@outlook.com>
2018-12-05 22:04:09 +0100

    DODANO BACKUP CHARTS  - NAPRAWY OPON +  ODRZUTY NA PRZEKROCZONĄ MASE. WPROWADZONO PRZYCISKI PREZENTUJACE DANE WG ZMIAN LUB NARASTAJĄCO ZA DOBE

--- js/mainPage.js
+++ js/mainPage.js
@@ -4,6 +4,7 @@ AmCharts.lastClick = 0; // last click timestamp
 AmCharts.doubleClickDuration = 300;
 
 var chartData;
+var repairChartData;
 var inspectionChartData;
 var curingChartData;
 
@@ -12,13 +13,15 @@ var curingScraps;
 var uniScraps;
 var shift;
 var shiftPlan;
+var repairTotal;
+var massTotal;
 
 var tableInterval;
 var fvmInterval;
+var scrapInterval;
 
 var scrapChartRefreshMode=0; // DOMYŚLNIE DANE W WYKRESIE PREZENTUJĄ BIEŻĄCĄ DOBĘ
-
-
+var scrapChartAutoUpdate = 1; //AUTOMATYCZNA AKTUALIZACJA
 
 //AKCJE DO PODWÓJNEGO/POJEDYNCZEGO KLIKNIECIA NA GLOWNY WYKRES
 {
@@ -58,24 +61,15 @@ var scrapChartRefreshMode=0; // DOMYŚLNIE DANE W WYKRESIE PREZENTUJĄ BIEŻĄC�
         AmCharts.lastClick = ts;
     }
 }
-var scrapInterval;
+
+
 $(document).ready(function() {
 
     //SCRAPS CHART INIT/UPDATE
-    $.getJSON('php/getScraps.php?mode='+scrapChartRefreshMode, function(data)
-    {  //alert('xxx');
-       chartData=data.DANE;
-       buildingScraps=data.BUILDING.qty;
-       curingScraps=data.CURING.qty;
-       uniScraps=data.UNI.qty;
-       scrapsChart();
-       updateDashboard();
-    });
+
+    updateDashboard();
 
     //MAIN TABLE INIT/UPDATE
-
-
-
     statsInterval = setInterval(function () {
         $.getJSON('php/getForecast.php', function(data)
         {
@@ -93,10 +87,10 @@ $(document).ready(function() {
             $('#fvm').html("<span class='mdl-layout--large-screen-only'>"+data.MAIN_TABLE.FVM_OUTPUT+" /</span> "+data.MAIN_TABLE.FVM_FORECAST+"<br><span style='font-size: 80%'>("+(data.MAIN_TABLE.FVM_FORECAST-data.MAIN_TABLE.CURING_PRED)+")</span>");
             $('#stock').html("<span class='mdl-layout--large-screen-only'>"+data.MAIN_TABLE.STOCK+"<br><span style='font-size: 80%'>("+(data.MAIN_TABLE.STOCK-data.MAIN_TABLE.LABO)+")</span>");
 
-            inspectionChartData=data.MAIN_TABLE.SUPPORT_CHARTS.INSPECTION;
-            inspectionChart();
-            curingChartData=data.MAIN_TABLE.SUPPORT_CHARTS.CURING;
-            curingChart();
+            // inspectionChartData=data.MAIN_TABLE.SUPPORT_CHARTS.INSPECTION;
+            // inspectionChart();
+            // curingChartData=data.MAIN_TABLE.SUPPORT_CHARTS.CURING;
+            // curingChart();
         });
         },30000);
 
@@ -145,13 +139,10 @@ function mainTableFill()
             $('#inspection').removeClass().addClass('negativeCell');
 
 
-
-
-
-        inspectionChartData=data.MAIN_TABLE.SUPPORT_CHARTS.INSPECTION;
-        inspectionChart();
-        curingChartData=data.MAIN_TABLE.SUPPORT_CHARTS.CURING;
-        curingChart();
+        // inspectionChartData=data.MAIN_TABLE.SUPPORT_CHARTS.INSPECTION;
+        // inspectionChart();
+        // curingChartData=data.MAIN_TABLE.SUPPORT_CHARTS.CURING;
+        // curingChart();
     });
 }
 
@@ -161,7 +152,7 @@ function scrapsChart() {
         chart.dataProvider = chartData;
         chart.categoryField = "defectNumber";
         chart.startDuration = 1;
-        chart.addTitle("TOTAL: "+(buildingScraps+uniScraps+curingScraps)+", CURING: "+curingScraps+" / BUILDING: "+buildingScraps+" / UNI: "+uniScraps+ " "+moment().format('HH:mm:ss'));
+       // chart.addTitle("TOTAL: "+(buildingScraps+uniScraps+curingScraps)+", CURING: "+curingScraps+" / BUILDING: "+buildingScraps+" / UNI: "+uniScraps+ " "+moment().format('HH:mm:ss'));
 
         // AXES
         // category
@@ -193,18 +184,46 @@ function scrapsChart() {
         chart.addListener('clickGraphItem', AmCharts.myClickHandler);
         chart.creditsPosition = "top-right";
         chart.write("scrapChart");
-
 }
 
-function resetChart()
-{
-    chart.dataProvider = chartData;
-    chart.titles[0].text = "TOTAL "+categoryMode+" SCRAPS BY "+ updateMode + " - "+totalScraps;
-    // remove the "Go back" label
-    chart.allLabels = [];
-    chart.marginBottom=120;
-    chart.validateData();
-    chart.animateAgain();
+function supportScrapsChart(supportChartData,supportChartContainer, chartTitle) {
+    // SERIAL CHART
+    chart = new AmCharts.AmSerialChart();
+    chart.dataProvider = supportChartData;
+    chart.categoryField = "defectNumber";
+    chart.startDuration = 1;
+    chart.addTitle(chartTitle+" "+moment().format('HH:mm:ss'));
+
+    // AXES
+    // category
+    var categoryAxis = chart.categoryAxis;
+    categoryAxis.labelRotation = 30;
+    categoryAxis.gridPosition = "start";
+
+
+    // GRAPH
+    var graph = new AmCharts.AmGraph();
+
+    graph.valueField = "qty";
+    graph.balloonText = "[[desc]]: </br><b>[[text]]</b>";
+    graph.labelText="[[qty]]";
+    graph.type = "column";
+    graph.lineAlpha = 0;
+    graph.fillAlphas = 1;
+    graph.colorField= "color";
+    chart.addGraph(graph);
+
+
+    // CURSOR
+    var chartCursor = new AmCharts.ChartCursor();
+    chartCursor.cursorAlpha = 0;
+    chartCursor.zoomable = false;
+    chartCursor.categoryBalloonEnabled = false;
+    chart.addChartCursor(chartCursor);
+
+    chart.addListener('clickGraphItem', AmCharts.myClickHandler);
+    chart.creditsPosition = "top-right";
+    chart.write(supportChartContainer);
 }
 
 function updateDashboard()
@@ -212,116 +231,12 @@ function updateDashboard()
     //scrap chart update
     fvmPerf();
     fvmInterval=setInterval(fvmPerf,300000);
-    scrapInterval=setInterval(function(){
-        $.getJSON('php/getScraps.php', function(data)
-        {
-            chartData=data.DANE;
-            buildingScraps=data.BUILDING.qty;
-            curingScraps=data.CURING.qty;
-            uniScraps=data.UNI.qty;
-            chart.titles[0].text="TOTAL: "+(buildingScraps+uniScraps+curingScraps)+", CURING: "+curingScraps+" / BUILDING: "+buildingScraps+" / UNI: "+uniScraps+ " "+moment().format('HH:mm:ss');
-            chart.validateData();
-            chart.animateAgain();
-        });
-    }, 300000) ;
+
+    scrapsShift();
+    scrapInterval=setInterval(scrapsShift,300000);
 
     mainTableFill();
     tableInterval=setInterval(mainTableFill, 300000) ;
-}
-
-
-function inspectionChart() {
-    chartInspection = new AmCharts.AmSerialChart();
-    chartInspection.dataProvider = inspectionChartData;
-    chartInspection.autoMargins = false;
-    chartInspection.marginTop = 0;
-    chartInspection.marginBottom = 0;
-    chartInspection.marginLeft = 0;
-    chartInspection.marginRight = 0;
-    chartInspection.categoryField="TIMESTAMP;"
-
-    // AXES
-    // category
-    var categoryAxis = chartInspection.categoryAxis;
-
-    // value
-    var valueAxis = new AmCharts.ValueAxis();
-    valueAxis.axisAlpha = 0;
-    valueAxis.inside = true;
-    valueAxis.dashLength = 3;
-    chartInspection.addValueAxis(valueAxis);
-
-    // GRAPH
-    graph = new AmCharts.AmGraph();
-    graph.type = "smoothedLine"; // this line makes the graph smoothed line.
-    graph.lineColor = "#4e9cd1";
-    graph.negativeLineColor = "#b63733"; // this line makes the graph to change color when it drops below 0
-    graph.lineThickness = 2;
-    graph.valueField = "PRED";
-    graph.balloonText = "[[TIMESTAMP]]:<br><b><span style='font-size:14px;'>[[value]]</span></b>";
-    chartInspection.addGraph(graph);
-
-    // CURSOR
-    var chartCursor = new AmCharts.ChartCursor();
-    chartCursor.cursorAlpha = 0;
-    chartCursor.cursorPosition = "mouse";
-    chartCursor.categoryBalloonDateFormat = "YYYY";
-    chartInspection.addChartCursor(chartCursor);
-
-    // SCROLLBAR
-    var chartScrollbar = new AmCharts.ChartScrollbar();
-    chartInspection.addChartScrollbar(chartScrollbar);
-
-    chartInspection.creditsPosition = "bottom-right";
-    // WRITE
-    chartInspection.write("inspectionChart");
-}
-
-function curingChart() {
-    chartCuring = new AmCharts.AmSerialChart();
-    chartCuring.dataProvider = curingChartData;
-    chartCuring.labelsEnabled = false;
-    chartCuring.autoMargins = false;
-    chartCuring.marginTop = 0;
-    chartCuring.marginBottom = 0;
-    chartCuring.marginLeft = 0;
-    chartCuring.marginRight = 0;
-
-    // AXES
-    // category
-    var categoryAxis = chartCuring.categoryAxis;
-
-    // value
-    var valueAxis = new AmCharts.ValueAxis();
-    valueAxis.axisAlpha = 0;
-    valueAxis.inside = true;
-    valueAxis.dashLength = 3;
-    chartCuring.addValueAxis(valueAxis);
-
-    // GRAPH
-    graph = new AmCharts.AmGraph();
-    graph.type = "smoothedLine"; // this line makes the graph smoothed line.
-    graph.lineColor = "#25d12f";
-    graph.negativeLineColor = "#b63733"; // this line makes the graph to change color when it drops below 0
-    graph.lineThickness = 2;
-    graph.valueField = "PRED";
-    graph.balloonText = "[[TIMESTAMP]]:<br><b><span style='font-size:14px;'>[[value]]</span></b>";
-    chartCuring.addGraph(graph);
-
-    // CURSOR
-    var chartCursor = new AmCharts.ChartCursor();
-    chartCursor.cursorAlpha = 0;
-    chartCursor.cursorPosition = "mouse";
-    chartCursor.categoryBalloonDateFormat = "YYYY";
-    chartCuring.addChartCursor(chartCursor);
-
-    // SCROLLBAR
-    var chartScrollbar = new AmCharts.ChartScrollbar();
-    chartCuring.addChartScrollbar(chartScrollbar);
-
-    chartCuring.creditsPosition = "bottom-right";
-    // WRITE
-    chartCuring.write("curingChart");
 }
 
 function fvmPerf()
@@ -370,3 +285,32 @@ function fvmPerf()
     }
     );
 };
+
+function scrapsShift()
+{
+    $.getJSON('php/getScraps.php?mode='+scrapChartRefreshMode, function(data)
+    {
+        var autoUpdateText='<span style="color: #1b7a00; text-underline-style: wave; ">AUTOUPDATE ON</span>';
+        chartData=data.DANE;
+        buildingScraps=data.BUILDING.qty;
+        curingScraps=data.CURING.qty;
+        uniScraps=data.UNI.qty;
+        repairTotal=data.REPAIR;
+        repairChartData=data.DANE_NAPRAWA;
+        //alert(data.DANE);
+        //$('#test').html(data.DANE[1]);
+        //chart.dataProvider(chartData);
+        scrapsChart();
+        supportScrapsChart(repairChartData,'backupChart1', "NAPRAWY");
+        supportScrapsChart(data.DANE_MASY,'backupChart2', "MASY");
+
+        if(!scrapChartAutoUpdate)
+            autoUpdateText='<span style="color: #9f0012; text-underline-style: wave; ">AUTOUPDATE OFF</span>';
+       // alert(autoUpdateText);
+            $("#scrapNumbers").html("TOTAL: "+(buildingScraps+uniScraps+curingScraps)+", CURING: "+curingScraps+" / BUILDING: "+buildingScraps+" / UNI: "+uniScraps+ "/ <span style='color: #1a237e'><u>>RPR: "+repairTotal+"</u></span> "+moment().format('HH:mm:ss')+" "+autoUpdateText);
+        //chart.titles[0].text="TOTAL: "+(buildingScraps+uniScraps+curingScraps)+", CURING: "+curingScraps+" / BUILDING: "+buildingScraps+" / UNI: "+uniScraps+ " "+moment().format('HH:mm:ss');
+        // chart.validateNow();
+        // chart.validateData();
+       // chart.animateAgain();
+    });
+}
--- php/getScraps.php
+++ php/getScraps.php
@@ -66,32 +66,95 @@
             $zmiana=3;
         }
 
-//DO TESTÓW
 
-        //NARASTAJĄCO DOBA
-        if(mode == 0)
+//DO TESTÓW - RÓŻNE TRYBY PRZEDSTAWIANIA DANYCH
+{
+    //NARASTAJĄCO DOBA
+    if ($_GET['mode'] == 0) {
+        if($h<=6)
+        {
+            $start=new DateTime();
+            $start->sub(new DateInterval('P1D'));
+            $start=date_format ( $start, 'y-m-d');
+            $end=new DateTime();
+            $end=date_format ( $end, 'y-m-d');
+        }
+        else{
+            $start=new DateTime();
+            $start=date_format ( $start, 'y-m-d');
+            $end = new DateTime();
+            $end->add(new DateInterval('P1D'));
+            $end = date_format($end, 'y-m-d');
+        }
+        $start_hr = 06;
+        $end_hr = 06;
+    }
+
+    //TYLKO ZMIANA1
+    else if ($_GET['mode'] == 1) {
+        if($h<6)
+        {
+            $start=new DateTime();
+            $start->sub(new DateInterval('P1D'));
+            $start=date_format ( $start, 'y-m-d');
+            $end=new DateTime();
+            $end->sub(new DateInterval('P1D'));
+            $end=date_format ( $end, 'y-m-d');
+        }
+        else{
+            $start=new DateTime();
+            $start=date_format ( $start, 'y-m-d');
+            $end = new DateTime();
+            $end = date_format($end, 'y-m-d');
+        }
+
+        $start_hr = 06;
+        $end_hr = 14;
+    }
+    //TYLKO ZMIANA2
+    else if ($_GET['mode'] == 2) {
+        if($h<6)
+        {
+            $start=new DateTime();
+            $start->sub(new DateInterval('P1D'));
+            $start=date_format ( $start, 'y-m-d');
+            $end=new DateTime();
+            $end->sub(new DateInterval('P1D'));
+            $end=date_format ( $end, 'y-m-d');
+        }
+        else{
+            $start=new DateTime();
+            $start=date_format ( $start, 'y-m-d');
+            $end = new DateTime();
+            $end = date_format($end, 'y-m-d');
+        }
+
+        $start_hr = 14;
+        $end_hr = 22;
+    } //ZMIANA3
+    else {
+        if($h>=22)
         {
             $end=new DateTime();
             $end->add(new DateInterval('P1D'));
             $end=date_format ( $end, 'y-m-d');
-            $start_hr=06;
-            $end_hr=6;
+            $start=new DateTime();
+            $start=date_format ( $start, 'y-m-d');
         }
-        //TYLKO ZMIANA1
-        else if (mode == 1)
+        if($h<6)
         {
-
+            $start=new DateTime();
+            $start->sub(new DateInterval('P1D'));
+            $start=date_format ( $start, 'y-m-d');
+            $end=new DateTime();
+            $end=date_format ( $end, 'y-m-d');
         }
-        //TYLKO ZMIANA2
-        else if(mode == 2)
-        {
+        $start_hr = 22;
+        $end_hr = 6;
+    }
+}
 
-        }
-        //ZMIANA3
-        else
-        {
 
-        }
 
         $scrapsSummary=array('BUILDING' => array('qty' =>0, 'color' =>'#f8fc00') , 'UNI' => array('qty' =>0, 'color' =>'#ef8904'), 'CURING' => array('qty' =>0, 'color' =>'#ef0404'));
         $dataArray=array();
@@ -136,13 +199,68 @@
                 $scrapsSummary[$row[4]]['qty']+=$row[2];
                 if($i<15)
                  array_push($dataArray,array("defectNumber" => $row[0],"qty"=>$row[2], "desc" =>$row[1], "text" => $row[3], "color" =>$scrapsSummary[$row[4]]['color']));
-
                 $i++;
             }
 
+            //WYKRES I DANE NT NAPRAW
+            $repairArray=array();
+			$repairSummary=0;
+            $stid = oci_parse($ffmes_connection,"select defect_num, long_desc, sum(per_machine),
+                                                        listagg(case when per_machine >=3 then tire_code || ' / ' || maszyna || '-' || per_machine || 'szt.' else null end, '</br>') within group(order by per_gt_code desc, per_machine desc) serie,
+                                                        case when defect_num  in (1, 2, 4, 5, 7, 8, 11, 49, 52, 53, 92, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 125, 126, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 176, 177, 178, 179, 180, 181, 182, 185, 197, 198, 199) then 'CURING'
+                                                        when defect_num in (141,142,143,144,145,146, 147,  148, 149, 150, 151, 152, 153, 154, 155, 156) then 'UNI' else 'BUILDING' end color
+                                                        from
+                                                        (
+                                                          select distinct tic tire_code, long_desc, defect_num,maszyna,
+                                                          count(tic) over (partition by tic, defect_num) per_gt_code,
+                                                          count(tic) over (partition by tic,defect_num, maszyna) per_machine
+                                                          from
+                                                          (
+                                                              select 
+                                                              to_char(transaction_date - interval '6' hour,'yy-mm-dd'),long_desc, df_entry_history.defect_num,tic,
+                                                              case when df_entry_history.defect_num  in (1, 2, 4, 5, 7, 8, 11, 49, 52, 53, 92, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 125, 126, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 176, 177, 178, 179, 180, 181, 182, 185, 197, 198, 199)
+                                                              then substr(prscav,1,3)
+                                                              else
+                                                              s2mach
+                                                              end maszyna
+                                                              from df_entry_history@plda5l2 left join df_defect_desc@plda5l2 on df_defect_desc.defect_num=df_entry_history.defect_num 
+                                                              left join barcode@sbs on df_entry_history.barcode=barcode.barcode
+                                                              where df_entry_history.defect_type='C' and df_defect_desc.defect_type='C' and disposition in (6) 
+                                                              and transaction_date between to_date('".$start." ".$start_hr.":00:00','yy-mm-dd hh24:mi:ss') and to_date('".$end." ".$end_hr.":00:00','yy-mm-dd hh24:mi:ss')   and transaction_type='INS'
+                                                          )
+                                                        )group by defect_num, long_desc order by sum(per_machine) desc");
+                oci_execute($stid);
+
+                $i=0;
+                while ($row = oci_fetch_array($stid, OCI_BOTH))
+                {
+                    $repairSummary+=$row[2];
+                    array_push($repairArray,array("defectNumber" => $row[0],"qty"=>$row[2], "desc" =>$row[1], "text" => $row[3], "color" =>"#4242f4"));
+                }
+
+          //MASY
+//
+        $massArray=array();
+        $massSummary=0;
+        $stid = oci_parse($ffmes_connection,"select cured_tire_code,count(barcode)
+                                from tw_entry@plda5l2 where cured_tire_code like 'PL-C%'
+                                and last_processed=1 and
+                                dstamp  between to_date('".$start." ".$start_hr.":00:00','yy-mm-dd hh24:mi:ss') and to_date('".$end." ".$end_hr.":00:00','yy-mm-dd hh24:mi:ss') and limits_position
+                                not in (1,-1,2,-2)
+                                group by cured_tire_code
+                                order by count(barcode) desc");
+        oci_execute($stid);
+
+
+        while ($row = oci_fetch_array($stid, OCI_BOTH))
+        {
+            $massSummary+=$row[1];
+            array_push($massArray,array("defectNumber" => substr($row[0],6,4),"qty"=>$row[1], "desc" =>$row[0], "text" => "", "color" =>"#682912"));
+        }
+
         //$dataArray=array(array_values($typeScrapsArray),$totalScrapsAmount);
         //print_r ($dataArray);
-        $dataArray=array_merge($scrapsSummary, array('DANE' => $dataArray));
+        $dataArray=array_merge($scrapsSummary, array('DANE' => $dataArray),array('DANE_NAPRAWA' =>$repairArray), array('REPAIR' => $repairSummary), array("DANE_MASY" => $massArray));
 		echo json_encode($dataArray,JSON_NUMERIC_CHECK);
 		oci_close($ffmes_connection);
 ?>
